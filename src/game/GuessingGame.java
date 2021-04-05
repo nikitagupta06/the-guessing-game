48b53595e9f6d5602b14cd1957e84e4a4023b593
nikitagupta <nikitagupta@192.168.29.134>
2021-04-05 23:30:40 +0530

    Using Binary Search

--- src/game/GuessingGame.java
+++ src/game/GuessingGame.java
@@ -1,55 +1,41 @@
 package game;
 
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-public class theGuessingGame {
+public class GuessingGame {
 
 	public static void main(String[] args) {
 
-		int inp = 0;
-
-		ArrayList<Integer> arr = new ArrayList<Integer>();
+		int guess = 0;
 		for (int i = 1; i <= 16; i++) {
-			arr.add(i);
+			System.out.print(i + " ");
 		}
-		System.out.println(arr);
-		int beg = 1;
-		int end = arr.size();
-
 		Random rand = new Random();
 		int val = rand.nextInt(16);
-		//System.out.println(val);
-
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a value between 1-16 : ");
-
-		while (arr.size() > 0) {
-
-			inp = sc.nextInt();
-
-			if (inp > val) {
-				end = inp - 1;
-				arr.clear();
+		System.out.println("\nEnter a value between 1-16 : ");
+		int beg = 1;
+		int end = 16;
+		while (beg <= end) {
+			guess = sc.nextInt();
+			if (guess > val) {
+				end = guess - 1;
+				System.out.println("My number is lower than " + guess + ". Guess lower!");
 				for (int i = beg; i <= end; i++) {
-					arr.add(i);
+					System.out.print(i + " ");
 				}
-				System.out.println("My number is lower than " + inp + ". Guess lower");
-
-			} else if (inp < val) {
-				beg = inp + 1;
-				arr.clear();
+			} else if (guess < val) {
+				beg = guess + 1;
+				System.out.println("My number is higher than " + guess + ". Guess higher!");
 				for (int i = beg; i <= end; i++) {
-					arr.add(i);
+					System.out.print(i + " ");
 				}
-				System.out.println("My number is higher than " + inp + ". Guess higher");
-
 			} else {
 				System.out.println("Yes! That's my number!");
 				break;
 			}
-			System.out.println(arr);
+			System.out.println();
 		}
 	}
 
